5a07938dc26083c2aaeaae73919ddbc2f1104ae9
Ali Ahmadi <ali.ahmadi.2002d@gmail.com>
2022-10-09 21:00:37 +0330

    Update adder.v

--- Adder/adder.v
+++ Adder/adder.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/22/2022 12:35:27 AM
-// Design Name: 
-// Module Name: adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module adder(A,B,Cin,Cout,Sum);
 input [3:0] A,B;
 input Cin;
